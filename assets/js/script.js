3d8fc18217d9503dcabba869e61ab82d038825a0
Felipe Gutierrez <feguti4@gmail.com>
2022-11-08 18:16:13 -0300

    Adicionando projeto semi-desenvolvido

---
+++ assets/js/script.js
@@ -0,0 +1,92 @@
+(function() {
+  "use strict"; // Start of use strict
+
+  var mainNav = document.querySelector('#mainNav');
+
+  if (mainNav) {
+
+    var navbarCollapse = mainNav.querySelector('.navbar-collapse');
+    
+    if (navbarCollapse) {
+      
+      var collapse = new bootstrap.Collapse(navbarCollapse, {
+        toggle: false
+      });
+      
+      var navbarItems = navbarCollapse.querySelectorAll('a');
+      
+      // Closes responsive menu when a scroll trigger link is clicked
+      for (var item of navbarItems) {
+        item.addEventListener('click', function (event) {
+          collapse.hide();
+        });
+      }
+    }
+
+    // Collapse Navbar
+    var collapseNavbar = function() {
+
+      var scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+
+      if (scrollTop > 100) {
+        mainNav.classList.add("navbar-shrink");
+      } else {
+        mainNav.classList.remove("navbar-shrink");
+      }
+    };
+    // Collapse now if page is not at top
+    collapseNavbar();
+    // Collapse the navbar when page is scrolled
+    document.addEventListener("scroll", collapseNavbar);
+  }
+
+  // bageutteBox init
+  if (document.getElementsByClassName('popup-gallery').length > 0) {
+    baguetteBox.run('.popup-gallery', { animation: 'slideIn' });
+  }
+
+})(); // End of use strict
+
+var popupViews = document.querySelectorAll('.popup-view');
+var popupBtns = document.querySelectorAll('.popup-btn');
+var closeBtns = document.querySelectorAll('.close-btn');
+
+//javascript for quick view button
+var popup = function (popupClick) {
+    popupViews[popupClick].classList.add('active');
+}
+
+popupBtns.forEach((popupBtn, i) => {
+    popupBtn.addEventListener("click", () => {
+        popup(i);
+    });
+});
+
+//javascript for close button
+closeBtns.forEach((closeBtn) => {
+    closeBtn.addEventListener("click", () => {
+        popupViews.forEach((popupView) => {
+            popupView.classList.remove('active');
+        });
+    });
+});
+
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += 375;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= 375;
+    })
+})
+
+
+
